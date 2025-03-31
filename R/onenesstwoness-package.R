f89a5d15d354bd9fe3da941af59e91609b78f271
jrafailov <jrafailov12@gmail.com>
2025-03-31 13:49:14 -0400

    verified homeology works with GxG + dependency statements

--- R/onenesstwoness-package.R
+++ R/onenesstwoness-package.R
@@ -1,17 +1,24 @@
 #' onenesstwoness - predictor of HRD phenotype
 #'
+#' This package provides tools for predicting HRD (Homologous Recombination Deficiency) phenotype 
+#' using various genomic and statistical methods. It integrates multiple libraries and utilities 
+#' to facilitate genomic data analysis and modeling.
+#'
+#' @name onenesstwoness-package
+#' @docType package
 #' @importFrom gUtils grl.in parse.gr gr.fix grl.pivot
 #' @import khtools
 #' @import randomForest
-#' @import data.table
-#' @import GenomicRanges
 #' @importFrom dplyr mutate
 #' @importFrom data.table fread
 #' @importFrom reshape2 dcast
 #' @importFrom signature.tools.lib HRDetect_pipeline
-#' @importFrom GxG homeology.wrapper
 #' @importFrom rtracklayer TwoBitFile
-#' @import BiocGenerics S4Vectors
+#' @import GxG
+#' @import BiocGenerics 
+#' @import S4Vectors
+#' @import GenomicRanges
+#' @import data.table
 #' @import gGnome
 #' @importFrom magrittr `%>%`
 registerS3method("merge", "data.table", data.table::merge.data.table)
